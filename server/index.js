f6383bc7376e307f1c84c53526622b3999a25412
BotyK <botykoutouan@gmail.com>
2022-09-01 14:39:33 -0500

    error function in the callback function testing rollbar

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ const app = express();
 // app.use(rollbar.errorHandler())
 
 app.use(cors());
+
 app.get('/api/turtles', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
     rollbar.log("Accessed HTML successfully");
 });
-
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+  }
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -28,40 +33,41 @@ const port = process.env.PORT || 4005;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
+// const turtles = ['Nemo', 'Frank']
+// app.get('/api/turtles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+//     res.status(200).send(turtles)
+//     rollbar.info("Someone pull up a turtle");
+// });
 
-app.get('/api/turtles', (req, res) => {
-    res.status(200).send(turtles)
-    rollbar.info("Someone pull up a turtle");
-});
-
-app.post('/api/turtles', (req, res) => {
-    let {name} = req.body
+// app.post('/api/turtles', (req, res) => {
+//     let {name} = req.body
  
-    const index = turtles.findIndex(turtle => {
-        return turtle === name
-    })
+//     const index = turtles.findIndex(turtle => {
+//         return turtle === name
+//     })
  
-    try {
-        if (index === -1 && name !== '') {
-            turtles.push(name)
-            rollbar.log("Turtles added successfully", {author: "Boty", type: "manual entry"});
-            res.status(200).send(turtles)
-        } else if (name === ''){
-             rollbar.error("No name provided")
-            res.status(400).send('You must enter a name.')
-        } else {
-             rollbar.error("Turtle already exist");
-            res.status(400).send('That turtle already exists.')
-        }
-    } catch (err) {
-        console.log(err)
-    }
- })
+//     try {
+//         if (index === -1 && name !== '') {
+//             turtles.push(name)
+//             rollbar.log("Turtles added successfully", {author: "Boty", type: "manual entry"});
+//             res.status(200).send(turtles)
+//         } else if (name === ''){
+//              rollbar.error("No name provided")
+//             res.status(400).send('You must enter a name.')
+//         } else {
+//              rollbar.error("Turtle already exist");
+//             res.status(400).send('That turtle already exists.')
+//         }
+//     } catch (err) {
+//         console.log(err)
+//     }
+//  })
  
- app.delete('/api/turtles/:index', (req, res) => {
-     const targetIndex = +req.params.index
+//  app.delete('/api/turtles/:index', (req, res) => {
+//      const targetIndex = +req.params.index
      
-     turtles.splice(targetIndex, 1)
-     rollbar.info("Turtles was deleted");
-     res.status(200).send(turtles)
- })
+//      turtles.splice(targetIndex, 1)
+//      rollbar.info("Turtles was deleted");
+//      res.status(200).send(turtles)
+//  })
